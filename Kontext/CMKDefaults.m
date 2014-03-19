64ebbe2ae8713a134b14516ed892be462ea9520b
Adam Durity <adurity@cmu.edu>
2014-03-19 13:05:50 -0400

    Adds Estimote UUID

--- Kontext/CMKDefaults.m
+++ Kontext/CMKDefaults.m
@@ -18,9 +18,11 @@ NSString *BeaconIdentifier = @"com.example.apple-samplecode.AirLocate";
     if(self)
     {
         // uuidgen should be used to generate UUIDs.
-        _supportedProximityUUIDs = @[[[NSUUID alloc] initWithUUIDString:@"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"],
-                                      [[NSUUID alloc] initWithUUIDString:@"5A4BCFCE-174E-4BAC-A814-092E77F6B7E5"],
-                                      [[NSUUID alloc] initWithUUIDString:@"74278BDA-B644-4520-8F0C-720EAF059935"]];
+        _supportedProximityUUIDs = @[
+            [[NSUUID alloc] initWithUUIDString:@"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"],
+            [[NSUUID alloc] initWithUUIDString:@"5A4BCFCE-174E-4BAC-A814-092E77F6B7E5"],
+            [[NSUUID alloc] initWithUUIDString:@"74278BDA-B644-4520-8F0C-720EAF059935"],
+            [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"]];
         _defaultPower = @-59;
     }
     
