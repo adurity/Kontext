3a12057e29915b397e47883f364cde60a0f552b9
Adam Durity <adurity@cmu.edu>
2014-03-23 18:14:11 -0400

    Creates tabbed interface and Event List view.

---
+++ Kontext/CMKEventTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMKEventTableViewController.h
+//  Kontext
+//
+//  Created by Adam Durity on 3/23/14.
+//  Copyright (c) 2014 Carnegie Mellon University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMKEventTableViewController : UITableViewController
+
+@end
