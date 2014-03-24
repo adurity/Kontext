1901b8cc2ef73c18684c4e6d475770ff1ad798fe
Adam Durity <adurity@cmu.edu>
2014-03-24 00:52:17 -0400

    Creates static schedule dynamically.

--- Kontext/CMKEventTableViewController.m
+++ Kontext/CMKEventTableViewController.m
@@ -10,6 +10,8 @@
 
 @interface CMKEventTableViewController ()
 
+@property NSDictionary *groupedEvents;
+
 @end
 
 @implementation CMKEventTableViewController
@@ -17,12 +19,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+    NSString *eventsDictionaryPath;
+    if ((eventsDictionaryPath = [[NSBundle mainBundle] pathForResource:@"Events" ofType:@"plist"])) {
+        self.groupedEvents = [NSDictionary dictionaryWithContentsOfFile:eventsDictionaryPath];
+    }
+    else
+    {
+        self.groupedEvents = [NSDictionary dictionary];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,71 +40,34 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return self.groupedEvents.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    NSArray *events = [self.groupedEvents allValues];
+    return [events[section] count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *sectionTitle;
+    NSArray *sectionKeys = [self.groupedEvents allKeys];
+
+    sectionTitle = sectionKeys[section];
+    return sectionTitle;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EventCell" forIndexPath:indexPath];
-    
-    cell.textLabel.text = @"Event Name";
-    cell.detailTextLabel.text = @"Room #101";
+
+    NSString *sectionKey = [self.groupedEvents allKeys][indexPath.section];
+    NSDictionary *event = self.groupedEvents[sectionKey][indexPath.row];
+    cell.textLabel.text = event[@"Name"];
+    cell.detailTextLabel.text = event[@"Room"];
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
